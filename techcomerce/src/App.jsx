1db9667cd5f41e829d81e4eff1191269d9ef6866
Ellison <ellison.m@escolar.ifrn.edu.br>
2025-01-21 21:37:54 -0300

    21/01;25

--- techcomerce/src/App.jsx
+++ techcomerce/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from './components/header';
 import './App.css';
+import Footer from './components/footer';
+
+
 
 function App(){
   return(
 
   <div className="app-container">
     <Header />
+    <Footer />
   </div>
   );
 }
